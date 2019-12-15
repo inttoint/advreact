23ef4d31b641f1c98505a4a5ae2f862c6e67f98c
Alina Voronina <skorokhodova.a5@gmail.com>
2019-12-15 13:41:08 +0300

    Добавлен лоадер
    Маппинг состояния loading в свойства компонента из соответствующего duck

--- src/components/routes/AuthPage.js
+++ src/components/routes/AuthPage.js
@@ -3,11 +3,13 @@ import SignInForm from "../auth/SignInForm";
 import SignUpForm from "../auth/SignUpForm";
 import { Route, NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { signUp } from '../../ducks/auth';
+import { signUp, moduleName } from '../../ducks/auth';
+import Loader from "../Loader";
 
 class AuthPage extends Component {
 
   render() {
+    const { loading } = this.props;
     return (
       <div>
         <h2>Auth Page</h2>
@@ -15,6 +17,7 @@ class AuthPage extends Component {
         <NavLink to="/auth/signup" activeStyle={{color: "red"}}>sign up</NavLink>
         <Route path="/auth/signin" render={() => <SignInForm onSubmit={this.handleSignIn}/>}/>
         <Route path="/auth/signup" render={() => <SignUpForm onSubmit={this.handleSignUp}/>}/>
+        { loading && <Loader /> }
       </div>
     );
   }
@@ -22,7 +25,9 @@ class AuthPage extends Component {
   handleSignIn = (values) => console.log("---", values);
   handleSignUp = ({ email, password }) => this.props.signUp(email, password);
 
-
 }
 
-export default connect(null, { signUp })(AuthPage);
+
+export default connect((state) => ({
+  loading: state[moduleName].loading
+}),{ signUp })(AuthPage);
