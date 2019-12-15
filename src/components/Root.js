4f7c27e1caebc1ce944b1cf41444f8e9ad15a534
Alina Voronina <skorokhodova.a5@gmail.com>
2019-12-15 14:08:38 +0300

    Реализация ProtectedRoute для авторизованных пользователей
    ProtectedRoute делает компонент доступным для авторизованного пользователя

--- src/components/Root.js
+++ src/components/Root.js
@@ -2,12 +2,13 @@ import React from "react";
 import { Route } from 'react-router-dom';
 import AdminPage from "./routes/AdminPage";
 import AuthPage from "./routes/AuthPage";
+import ProtectedRoute from "./common/ProtectedRoute";
 
 
 const Root = () => {
   return (
     <div>
-      <Route path="/admin" component={AdminPage}/>
+      <ProtectedRoute path="/admin" component={AdminPage}/>
       <Route path="/auth" component={AuthPage}/>
     </div>
   );
