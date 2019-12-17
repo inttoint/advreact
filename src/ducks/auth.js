834b79ca4817f4b2a758fd3623404c5e54c94417
Alina Voronina <skorokhodova.a5@gmail.com>
2019-12-17 23:33:57 +0300

    Реализация функциональности signOut через сагу, обрабатывающую каждый экшн SIGN_OUT_REQUEST

--- src/ducks/auth.js
+++ src/ducks/auth.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase';
 import { appName } from '../config';
 import { Record } from 'immutable';
-import { all, call, take, put, cps } from 'redux-saga/effects';
+import { all, call, take, put, cps, takeEvery } from 'redux-saga/effects';
 
 const ReducerRecord = Record({
   user: null,
@@ -14,6 +14,8 @@ export const SIGN_UP_REQUEST = `${appName}/${moduleName}/SIGN_UP_REQUEST`;
 export const SIGN_UP_SUCCESS = `${appName}/${moduleName}/SIGN_UP_SUCCESS`;
 export const SIGN_UP_ERROR = `${appName}/${moduleName}/SIGN_UP_ERROR`;
 export const SIGN_IN_SUCCESS = `${appName}/${moduleName}/SIGN_IN_SUCCESS`;
+export const SIGN_OUT_REQUEST = `${appName}/${moduleName}/SIGN_OUT_REQUEST`;
+export const SIGN_OUT_SUCCESS = `${appName}/${moduleName}/SIGN_OUT_SUCCESS`;
 
 
 export default function reducer(state = new ReducerRecord(), action) {
@@ -23,7 +25,7 @@ export default function reducer(state = new ReducerRecord(), action) {
     case SIGN_UP_REQUEST:
       return state.set('loading', true);
 
-    case SIGN_UP_SUCCESS:
+    case SIGN_IN_SUCCESS:
       return state
         .set('loading', false)
         .set('user', payload.user)
@@ -33,6 +35,8 @@ export default function reducer(state = new ReducerRecord(), action) {
       return state
         .set('loading', false)
         .set('error', error);
+    case SIGN_OUT_SUCCESS:
+      return new ReducerRecord();
     default:
       return state;
   }
@@ -42,10 +46,16 @@ export function signUp(email, password) {
   return {
     type: SIGN_UP_REQUEST,
     payload: { email, password }
-  }
+  };
 }
 
-export function* signUpSaga() {
+export function signOut() {
+  return {
+    type: SIGN_OUT_REQUEST
+  };
+}
+
+export function * signUpSaga() {
   const auth = firebase.auth();
 
   while (true) {
@@ -67,22 +77,35 @@ export function* signUpSaga() {
 }
 
 export const watchStatusChange = function * () {
-const auth = firebase.auth();
+  const auth = firebase.auth();
 
-try {
-  yield cps([auth, auth.onAuthStateChanged]);
-} catch (user) {
-  yield put({
-    type: SIGN_IN_SUCCESS,
-    payload: { user }
-  });
-}
+  try {
+    yield cps([auth, auth.onAuthStateChanged]);
+  } catch (user) {
+    yield put({
+      type: SIGN_IN_SUCCESS,
+      payload: { user }
+    });
+  }
+};
+
+export const signOutSaga = function * () {
+  const auth = firebase.auth();
+  try {
+    yield call([auth, auth.signOut]);
+    yield put({
+      type: SIGN_OUT_SUCCESS
+    });
+  } catch (_) {
+
+  }
 };
 
 export const saga = function * () {
   yield all([
     signUpSaga(),
-    watchStatusChange()
+    watchStatusChange(),
+    takeEvery(SIGN_OUT_REQUEST, signOutSaga)
   ]);
 };
 
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,14 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
 import createRootReducer from "./reducer";
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
 import { routerMiddleware } from 'connected-react-router';
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from "../redux/saga";
 import history from '../history';
 
 const sagaMiddleware = createSagaMiddleware();
-const enhancer = applyMiddleware(sagaMiddleware, routerMiddleware(history), thunk, logger);
+const enhancer = applyMiddleware(sagaMiddleware, routerMiddleware(history), logger);
 
 const store = createStore(
   createRootReducer(history),
