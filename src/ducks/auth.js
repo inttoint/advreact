74a4a93bddc7549066b1d00581fb7e3aca33a19f
Alina Voronina <skorokhodova.a5@gmail.com>
2019-12-17 14:53:16 +0300

    Добавление саги signUp
    Использование бесконечной саги, которая реагирует на action REQUEST и вызывает createUser из контекста firebase, с параметрами. Yield дождется ответа от промиса и вернет юзера. Ошибки отлавливаются через try-catch

--- src/ducks/auth.js
+++ src/ducks/auth.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase';
 import { appName } from '../config';
 import { Record } from 'immutable';
+import { all, call, take, put } from 'redux-saga/effects';
 
 const ReducerRecord = Record({
   user: null,
@@ -37,19 +38,53 @@ export default function reducer(state = new ReducerRecord(), action) {
 };
 
 export function signUp(email, password) {
-  return (dispatch) => {
-    dispatch({
-      type: SIGN_UP_REQUEST
-    });
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then(user => dispatch({
-        type:SIGN_UP_SUCCESS,
+  return {
+    type: SIGN_UP_REQUEST,
+    payload: { email, password }
+  }
+}
+
+export function* signUpSaga() {
+  const auth = firebase.auth();
+
+  while (true) {
+    const {payload: {email, password}} = yield take(SIGN_UP_REQUEST);
+
+    try {
+      const user = yield call([auth, auth.createUserWithEmailAndPassword], email, password);
+      yield put({
+        type: SIGN_UP_SUCCESS,
         payload: user
-      }))
-      .catch(error => dispatch({
+      });
+    } catch (error) {
+      yield put({
         type: SIGN_UP_ERROR,
         error
-      }));
-  };
+      });
+    }
+  }
 }
+
+// export function signUp(email, password) {
+//   return (dispatch) => {
+//     dispatch({
+//       type: SIGN_UP_REQUEST
+//     });
+//     firebase.auth().createUserWithEmailAndPassword(email, password)
+//       .then(user => dispatch({
+//         type:SIGN_UP_SUCCESS,
+//         payload: user
+//       }))
+//       .catch(error => dispatch({
+//         type: SIGN_UP_ERROR,
+//         error
+//       }));
+//   };
+// }
+
+export const saga = function * () {
+  yield all([
+    signUpSaga()
+  ]);
+};
 
