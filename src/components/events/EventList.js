c1530d14b87aded61e0ec34e830134305af1e711
Alina Voronina <skorokhodova.a5@gmail.com>
2019-12-21 00:22:52 +0300

    Добавление таблицы эвентов и отображение лоадера

--- src/components/events/EventList.js
+++ src/components/events/EventList.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { eventListSelector, fetchAll } from "../../ducks/events";
+import { eventListSelector, moduleName, fetchAll } from "../../ducks/events";
+import Loader from '../common/Loader'
 
 class EventList extends Component {
 
@@ -9,13 +10,38 @@ class EventList extends Component {
   }
 
   render() {
-    console.log(this.props.events);
+    const { loading } = this.props;
+
+    if (loading) return <Loader />;
+
     return (
-      <div/>
+      <div>
+        <table>
+          <tbody>
+          { this.getRows() }
+          </tbody>
+        </table>
+      </div>
     );
+  }
+
+  getRows() {
+    return this.props.events.map(this.getRow);
+  }
+
+  getRow(event) {
+    return (
+      <tr key={event.uid}>
+        <td>{event.title}</td>
+        <td>{event.where}</td>
+        <td>{event.month}</td>
+      </tr>
+    )
   }
 }
 
+
 export default connect(state => ({
-  events: eventListSelector(state)
+  events: eventListSelector(state),
+  loading: state[moduleName].loading
 }), { fetchAll })(EventList);
