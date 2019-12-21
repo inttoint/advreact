7d49f4d864cd8ef33cc936dcea4fc8822f620a3f
Alina Voronina <skorokhodova.a5@gmail.com>
2019-12-21 15:10:02 +0300

    Больше тестов

--- src/components/events/EventList.test.js
+++ src/components/events/EventList.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import events from "../../mocks/conferences";
 import { EventList } from "./EventList";
 import Loader from "../common/Loader";
@@ -18,7 +18,13 @@ describe('EventList container', () => {
 
 
   describe('EventList container initial', () => {
+    const mockFetchAll = jest.fn();
+    const nextProps = { ...props, fetchAll: mockFetchAll };
+    mount(<EventList {...nextProps} />);
 
+    it ('should request fetch data', () => {
+      expect(mockFetchAll).toHaveBeenCalled();
+    });
   });
 
   describe('EventList container loading', () => {
@@ -38,6 +44,21 @@ describe('EventList container', () => {
 
       const rows = container.find('.test--event-list__row');
       expect(rows.length).toEqual(testEvents.length);
+    });
+
+    it('should select event', () => {
+      let selected = null;
+      const selectEvent = (uid) => selected = uid;
+      const nextProps = {
+        ...props,
+        events: testEvents,
+        selectEvent
+      };
+
+      const container = mount(<EventList { ...nextProps } />);
+      container.find('.test--event-list__row').first().simulate('click');
+
+      // expect(selected).toEqual(testEvents[0].uid);
     });
   });
 
