d217d784cadd78d867efd06008bf2881f3cc2214
Alina Voronina <skorokhodova.a5@gmail.com>
2019-12-29 14:45:39 +0300

    Описываю, как реагировать на событие сбрасывания чего-то компонент selectedEventCard

--- src/components/events/SelectedEventCard.js
+++ src/components/events/SelectedEventCard.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { DropTarget } from 'react-dnd';
+import {useDragSourceMonitor} from "react-dnd/lib/hooks/internal/drag";
 
 const SelectedEventCard = (props) => {
   const { connectDropTarget } = props;
   const { title, when, where } = props.event;
+
   return connectDropTarget(
     <div>
       <h3>{title}</h3>
@@ -13,7 +15,12 @@ const SelectedEventCard = (props) => {
 };
 
 const spec = {
+  drop(props, monitor) {
+    const personUid = monitor.getItem().uid;
+    const eventUid = props.event.uid;
 
+    console.log(`--> person: ${personUid} event:${eventUid}`);
+  }
 };
 
 const collect = (connect, monitor) => ({
