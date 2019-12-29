82d83189815b310c314183b110b1f71ebcf3b5a5
Alina Voronina <skorokhodova.a5@gmail.com>
2019-12-29 14:07:42 +0300

    Превращение PersonCard в draggable компонент
    Используются минимальные настройки. Больше - в доке

--- src/components/people/PersonCard.js
+++ src/components/people/PersonCard.js
@@ -1,13 +1,32 @@
 import React from "react";
+import { DragSource } from 'react-dnd';
 
 const PeopleCard = (props) => {
-  const { person: { firstName, lastName, email }, style } = props;
-  return (
-    <div style={{width: 200, height: 100, ...style}}>
+  const {
+    person: { firstName, lastName, email },
+    style, connectDragSource, isDragging } = props;
+  const draggStyle = {
+    backgroundColor: isDragging ? 'gray' : 'white'
+  };
+  return connectDragSource(
+    <div style={{width: 200, height: 100, ...draggStyle, ...style}}>
       <h3>{firstName}&nbsp;{lastName}</h3>
       <p>{email}</p>
     </div>
   );
 };
 
-export default PeopleCard;
+const spec = {
+  beginDrag(props) {
+    return {
+      uid: props.person.uid
+    }
+  }
+};
+
+const collect = (connect, monitor) => ({
+  connectDragSource: connect.dragSource(),
+  isDragging: monitor.isDragging()
+});
+
+export default DragSource('person', spec, collect)(PeopleCard);
