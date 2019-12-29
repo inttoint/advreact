20ea6572ac24e932d1692076613f99ee74640068
Alina Voronina <skorokhodova.a5@gmail.com>
2019-12-29 23:19:58 +0300

    Убрано стандартное dnd превью
    Теперь при перестаскивании - пусто

--- src/components/people/PersonCard.js
+++ src/components/people/PersonCard.js
@@ -1,20 +1,28 @@
-import React from "react";
+import React, {Component} from "react";
 import { DragSource } from 'react-dnd';
+import { getEmptyImage } from 'react-dnd-html5-backend';
 
-const PeopleCard = (props) => {
-  const {
-    person: { firstName, lastName, email },
-    style, connectDragSource, isDragging } = props;
-  const draggStyle = {
-    backgroundColor: isDragging ? 'gray' : 'white'
-  };
-  return (
-    <div style={{width: 200, height: 100, ...draggStyle, ...style}}>
-      {connectDragSource(<h3>{firstName}&nbsp;{lastName}</h3>)}
-      <p>{email}</p>
-    </div>
-  );
-};
+class PeopleCard extends Component {
+
+  componentDidMount() {
+    this.props.connectPreview(getEmptyImage());
+  }
+
+  render() {
+    const {
+      person: { firstName, lastName, email },
+      style, connectDragSource, isDragging } = this.props;
+    const draggStyle = {
+      backgroundColor: isDragging ? 'gray' : 'white'
+    };
+    return (
+      <div style={{width: 200, height: 100, ...draggStyle, ...style}}>
+        {connectDragSource(<h3>{firstName}&nbsp;{lastName}</h3>)}
+        <p>{email}</p>
+      </div>
+    );
+  }
+}
 
 const spec = {
   beginDrag(props) {
@@ -33,6 +41,7 @@ const spec = {
 
 const collect = (connect, monitor) => ({
   connectDragSource: connect.dragSource(),
+  connectPreview: connect.dragPreview(),
   isDragging: monitor.isDragging()
 });
 
