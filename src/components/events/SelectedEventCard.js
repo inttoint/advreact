449f61200ced7790bd85b30684204551cd165b14
Alina Voronina <skorokhodova.a5@gmail.com>
2019-12-29 23:02:27 +0300

    отображение людей на карточке эвента

--- src/components/events/SelectedEventCard.js
+++ src/components/events/SelectedEventCard.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { DropTarget } from 'react-dnd';
 import {connect} from "react-redux";
-import {addEventToPerson} from "../../ducks/people";
+import {addEventToPerson, peopleListSelector} from "../../ducks/people";
 
 const SelectedEventCard = (props) => {
-  const { connectDropTarget, canDrop, hovered } = props;
+  const { connectDropTarget, canDrop, hovered, people } = props;
   const { title, when, where } = props.event;
+
+  const peopleElement = people && (
+    <div style={{color: 'blue'}}>
+      <p>{people.map(person => person.email).join(', ')}</p>
+    </div>
+  );
 
   const dropStyle = {
     border: `2px solid ${canDrop ? 'green' : 'white'}`,
@@ -15,6 +21,7 @@ const SelectedEventCard = (props) => {
     <div style={dropStyle}>
       <h3>{title}</h3>
       <p>{where}, {when}</p>
+      {peopleElement}
     </div>
   );
 };
@@ -36,4 +43,6 @@ const collect = (connect, monitor) => ({
   hovered: monitor.isOver()
 });
 
-export default connect(null, { addEventToPerson })(DropTarget(['person'], spec, collect)(SelectedEventCard));
+export default connect((state, props) => ({
+  people: peopleListSelector(state).filter(person => person.events.includes(props.event.uid))
+}), { addEventToPerson })(DropTarget(['person'], spec, collect)(SelectedEventCard));
