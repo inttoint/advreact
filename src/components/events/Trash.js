5abc43544c2ed89259fc6cfd2e6ab45e58befa7d
Alina Voronina <skorokhodova.a5@gmail.com>
2019-12-31 22:44:01 +0300

    Добавлены стили для подсветки и перекрытия

--- src/components/events/Trash.js
+++ src/components/events/Trash.js
@@ -2,14 +2,16 @@ import React from "react";
 import { DropTarget } from 'react-dnd';
 
 const Trash = (props) => {
-  const { connectDropTarget } = props;
+  const { connectDropTarget, highlighted, hovered } = props;
 
   const style = {
     display: 'inline-block',
     float: 'right',
-    border: "2px solid red",
-    borderRadius: '10% 10% 50% 50%',
-    color: 'red'
+    padding: '0 30px',
+    border: `2px solid ${highlighted ? 'darkgreen' : 'red'}`,
+    borderRadius: '0 0 50% 50%',
+    color: highlighted ? 'darkgreen' : 'red',
+    backgroundColor: hovered ? 'silver' : 'white'
   };
 
   return connectDropTarget(
@@ -28,7 +30,9 @@ const spec = {
 };
 
 const collect = (connect, monitor) => ({
-  connectDropTarget: connect.dropTarget()
+  connectDropTarget: connect.dropTarget(),
+  highlighted: monitor.canDrop(),
+  hovered: monitor.isOver()
 });
 
 export default DropTarget(['event'], spec, collect)(Trash);
