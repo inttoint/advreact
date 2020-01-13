bd5aa226df43252e99f1ddfda65955097201929a
Alina Voronina <skorokhodova.a5@gmail.com>
2020-01-13 14:06:29 +0300

    TransitionMotion обертка

--- src/components/events/SelectedEvents.js
+++ src/components/events/SelectedEvents.js
@@ -2,16 +2,39 @@ import React, { Component } from "react";
 import { selectedEventsSelector } from '../../ducks/events';
 import { connect } from "react-redux";
 import SelectedEventCard from "./SelectedEventCard";
+import { TransitionMotion } from 'react-motion';
 
 
 class SelectedEvents extends Component {
  render() {
-   const { events } = this.props;
    return (
-     <div>
-       {events.map(event => event && <SelectedEventCard event={event} key={event.uid}/>)}
-     </div>
+     <TransitionMotion
+       styles={this.getStyles()}
+     >
+       { interpolatedStyles => (
+         <div>
+           {
+             interpolatedStyles.map(config => (
+               <div style={config.style} key={config.key}>
+                 <SelectedEventCard event={config.data} />
+               </div>
+             ))
+           }
+         </div>
+       )
+       }
+     </TransitionMotion>
    );
+ }
+
+ getStyles() {
+   return this.props.events.map(event => ({
+     style: {
+       opacity: 1
+     },
+     key: event.uid,
+     data: event
+   }));
  }
 }
 
