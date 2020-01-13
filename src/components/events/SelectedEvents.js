04836bb2f849d6f25f15ee61a19415eb9d7be0d9
Alina Voronina <skorokhodova.a5@gmail.com>
2020-01-13 13:58:46 +0300

    Преобразование функционального компонента к классу

--- src/components/events/SelectedEvents.js
+++ src/components/events/SelectedEvents.js
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import { selectedEventsSelector } from '../../ducks/events';
 import { connect } from "react-redux";
 import SelectedEventCard from "./SelectedEventCard";
 
 
-const SelectedEvents = (props) => {
-  const { events } = props;
-  return (
-    <div>
-      {events.map(event => event && <SelectedEventCard event={event} key={event.uid}/>)}
-    </div>
-  );
-};
+class SelectedEvents extends Component {
+ render() {
+   const { events } = this.props;
+   return (
+     <div>
+       {events.map(event => event && <SelectedEventCard event={event} key={event.uid}/>)}
+     </div>
+   );
+ }
+}
 
 export default connect(state => ({
   events: selectedEventsSelector(state)
