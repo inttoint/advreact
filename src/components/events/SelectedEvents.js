c29452506e11b395ed7a096d68bc6332bb4438e3
Alina Voronina <skorokhodova.a5@gmail.com>
2020-01-13 14:13:32 +0300

    Добавлено плавное появление и исчезновение элемента списка

--- src/components/events/SelectedEvents.js
+++ src/components/events/SelectedEvents.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { selectedEventsSelector } from '../../ducks/events';
 import { connect } from "react-redux";
 import SelectedEventCard from "./SelectedEventCard";
-import { TransitionMotion } from 'react-motion';
+import { TransitionMotion, spring } from 'react-motion';
 
 
 class SelectedEvents extends Component {
@@ -10,6 +10,8 @@ class SelectedEvents extends Component {
    return (
      <TransitionMotion
        styles={this.getStyles()}
+       willLeave={this.willLeave}
+       willEnter={this.willEnter}
      >
        { interpolatedStyles => (
          <div>
@@ -30,12 +32,21 @@ class SelectedEvents extends Component {
  getStyles() {
    return this.props.events.map(event => ({
      style: {
-       opacity: 1
+       opacity: spring(1, { stiffness: 50 })
      },
      key: event.uid,
      data: event
    }));
  }
+
+ willLeave = () => ({
+   opacity: spring(0, { stiffness: 100 })
+ });
+
+ willEnter = () => ({
+   opacity: 0
+ })
+
 }
 
 export default connect(state => ({
